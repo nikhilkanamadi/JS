e21e7cf683c278831914cdc30daca4dc13e6e4b7
nikhilkanamadi <kanamadinikhil@gmail.com>
2023-09-21 16:40:05 +0530

    First Commit

---
+++ introduction.js
@@ -0,0 +1,22 @@
+
+// alert('Hello,World!');
+console.log('Hello,World!');
+//prompt('Hello,World!');
+
+
+
+let message = 'Hello, World!';
+
+console.log(message);
+
+let user = 'John';
+let age = 27;
+let company = 'EY';
+console.log(user);
+console.log(age);
+console.log(company);
+console.log(` The user details ${user} age ${age} company name ${company}`);
+
+// reassigning the value
+message = 123457
+console.log(message);
